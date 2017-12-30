c077052f13a8152cfb7583f3a1672cc7d34ab5bc
Doan The Anh <doantheanh172tb@gmail.com>
2017-12-30 21:52:10 +0700

    [Styling] Styling header

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -6,19 +6,24 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image } = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
+    const {
+        headerTextStyle,
+        thumbnailContainerStyle,
+        thumbnailStyle,
+        headerContentStyle
+    } = styles;
 
     return (
         <Card>
             <CardSection>
-                <View >
+                <View style={thumbnailContainerStyle}>
                     <Image
                         style={thumbnailStyle}
                         source={{ uri: thumbnail_image }}
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
@@ -31,9 +36,18 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle: {
+        fontSize: 19
+    },
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
     }
 }
 
