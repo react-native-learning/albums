e475c1087f96b601329b2b68a6aa91908760cf8a
Doan The Anh <doantheanh172tb@gmail.com>
2017-12-30 16:48:36 +0700

    [componentWillMount] set state in componentWillMount

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+
+    state = {
+        albums: []
+    };
+
+    componentWillMount() {
+        console.log('AlbumList::componentWillMount');
+        // debugger
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then((res) => {
+                // console.log(res);
+                this.setState({
+                    albums: res.data
+                })
+            })
+    }
+
     render() {
+        console.log('AlbumList::render');
+        console.log('AlbumList::albums', this.state);
+
         return (
             <View>
                 <Text>AlbumList !!</Text>
