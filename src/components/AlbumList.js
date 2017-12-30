4c771210fd0c1220d26ff33403d0ef8265aa98cf
Doan The Anh <doantheanh172tb@gmail.com>
2017-12-30 22:23:24 +0700

    styling

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 
 import AlbumDetail from './AlbumDetail';
@@ -33,9 +33,9 @@ class AlbumList extends Component {
         // console.log('AlbumList::albums', this.state);
 
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 };
