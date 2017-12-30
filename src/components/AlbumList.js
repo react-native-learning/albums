d249d4bede44b3ed9d50c50d351c4044220d774f
Doan The Anh <doantheanh172tb@gmail.com>
2017-12-30 15:18:40 +0700

    [albumlist component] function to class

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>AlbumList !!</Text>
-        </View>
-    );
-}
+class AlbumList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>AlbumList !!</Text>
+            </View>
+        );
+    }
+};
 
 export default AlbumList;
